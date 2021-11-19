3cc14dde1b4d638e3921d857599cf70b209f1a41
Natalia Pavlova <pavlova_li@ukr.net>
2021-11-19 23:35:28 +0200

    Updates

---
+++ precache-manifest.7184bc41303b0621d9696a70d1a66ba8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf0eaf3468b9a57a5c7755f43c6a2a5",
+    "url": "/Lia-Portfolio/index.html"
+  },
+  {
+    "revision": "c4c58eef8b6deb8c2b8b",
+    "url": "/Lia-Portfolio/static/css/main.41f56633.chunk.css"
+  },
+  {
+    "revision": "4681c47a117de700af87",
+    "url": "/Lia-Portfolio/static/js/2.7b3d4c16.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Lia-Portfolio/static/js/2.7b3d4c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4c58eef8b6deb8c2b8b",
+    "url": "/Lia-Portfolio/static/js/main.2d4b64e5.chunk.js"
+  },
+  {
+    "revision": "93c7f8235cbf5442ced2",
+    "url": "/Lia-Portfolio/static/js/runtime-main.6ffe0ac4.js"
+  },
+  {
+    "revision": "4b8d99960b4424a0774d487fab4055de",
+    "url": "/Lia-Portfolio/static/media/Natalia_Pavlova_resume.4b8d9996.pdf"
+  },
+  {
+    "revision": "4769ad92c4666bb4cd4ff6f2934ba428",
+    "url": "/Lia-Portfolio/static/media/aec.4769ad92.jpg"
+  },
+  {
+    "revision": "4e15160a63cabe4d08ffc13508d36437",
+    "url": "/Lia-Portfolio/static/media/certificate.4e15160a.png"
+  },
+  {
+    "revision": "82f443e685f096020f678d612b341b89",
+    "url": "/Lia-Portfolio/static/media/lia.82f443e6.jpg"
+  }
+]);

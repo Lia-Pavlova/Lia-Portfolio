1b12e9398595bdf1d88d9bca5ca7b89b0a0c470a
Natalia Pavlova <pavlova_li@ukr.net>
2021-11-19 20:17:40 +0200

    Updates

--- precache-manifest.9b0241194abba730c24168bfda40a345.js
+++ precache-manifest.9b0241194abba730c24168bfda40a345.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e6b4157b3a1dc6eb072484ba29a12e43",
+    "revision": "5f9ec87ef7cb7b1c73788f3a409216f2",
     "url": "/Lia-Portfolio/index.html"
   },
   {
-    "revision": "e7198a424633c25e21c5",
+    "revision": "9b00b0c43012422bb59d",
     "url": "/Lia-Portfolio/static/css/main.6f736ae1.chunk.css"
   },
   {
-    "revision": "3ad411f17a61659e53a9",
-    "url": "/Lia-Portfolio/static/js/2.a41ca70e.chunk.js"
+    "revision": "5c3f0454bb39419a5538",
+    "url": "/Lia-Portfolio/static/js/2.477badea.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/Lia-Portfolio/static/js/2.a41ca70e.chunk.js.LICENSE.txt"
+    "url": "/Lia-Portfolio/static/js/2.477badea.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e7198a424633c25e21c5",
-    "url": "/Lia-Portfolio/static/js/main.9ddb41d7.chunk.js"
+    "revision": "9b00b0c43012422bb59d",
+    "url": "/Lia-Portfolio/static/js/main.6a8d102b.chunk.js"
   },
   {
     "revision": "93c7f8235cbf5442ced2",
     "url": "/Lia-Portfolio/static/js/runtime-main.6ffe0ac4.js"
+  },
+  {
+    "revision": "78d24cb1a625e8f5df0e9d92bfb58d70",
+    "url": "/Lia-Portfolio/static/media/Lia_Pavlova_CV.78d24cb1.pdf"
   },
   {
     "revision": "82f443e685f096020f678d612b341b89",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Lia-Portfolio/precache-manifest.d75ad18b4917a904769a27cc21e6837a.js"
+  "/Lia-Portfolio/precache-manifest.9b0241194abba730c24168bfda40a345.js"
 );
 
 self.addEventListener('message', (event) => {
